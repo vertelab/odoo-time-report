42c16e7537b5f9403c159f19c1ccc7aee49fb264
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-10-30 13:22:39 +0100

    T/6250 missing license Update __manifest__.py

--- time_report_automation/__manifest__.py
+++ time_report_automation/__manifest__.py
@@ -24,6 +24,7 @@
     'author': 'Vertel AB',
     'depends': ['hr_timesheet','hr_timesheet_sheet'],
     'version': '1.0',
+    'license': 'AGPL-3',
     'description': """
     Adds a cron job that creates time sheets for all employees that have the field shoud_time_report set to true.
     """,
